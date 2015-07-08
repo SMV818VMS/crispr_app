7c0ad8362333d08670502dd9f94f984f5e1d48a6
alvaro <alvaroabascar@gmail.com>
2015-07-08 21:58:08 +0200

    version 0.2

--- app/main_app/views.py
+++ app/main_app/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, send_from_directory, url_for
+from flask import Blueprint, render_template, send_from_directory, url_for, request
 import pandas as pd
 import os
 
@@ -10,7 +10,22 @@ main_app = Blueprint('main_app', __name__,
 def main():
     path_to_table = '/home/alvaro/Repos/crispr_app/app/main_app/static/total_processed.txt'
     df = pd.read_csv(path_to_table, sep='\t', index_col=None)
-    return render_template("index.html", dataframe=df)
+    minimum = request.args.get('min', '')
+    maximum = request.args.get('max', '')
+    column = request.args.get('column', '')
+
+    if not column in df.columns:
+        return "Wrong column: {}".format(column)
+
+    try:
+        minimum = int(float(minimum))
+        maximum = int(float(maximum))
+    except:
+        return "Minimum value and maximum value must be numbers"
+
+    df = df[(df[column] >= minimum) & (df[column] <= maximum)]
+
+    return render_template("index.html", dataframe=df[:500])
 
 @main_app.route('/sayhello/<name>')
 def sayhello(name):
